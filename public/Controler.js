3ec0072e3c7957f942735f48f4ff66d13c85fe77
bwinslawski <wisla162@gmail.com>
2014-06-03 15:04:34 +0200

    dodanie sortowania

---
+++ public/Controler.js
@@ -0,0 +1,12 @@
+﻿var APP = angular.module('APP', []);
+APP.ApplicationCtrl = function ($scope) {
+	$.getJSON( "data.json", function(data) {
+					$scope.$apply(function(){
+					$scope.friends = data['vid'];
+					});
+				})
+				$scope.predicate = '-price';
+};
+
+
+// http://mrzepinski.pl/angularjs-5-ng-repeat.html  <- filtry
